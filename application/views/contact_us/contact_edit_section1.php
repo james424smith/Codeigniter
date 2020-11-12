d7953cae0d6d7863879d1ebc3d6494b7c6837396
eliekassis-dev <eliekassis.dev@gmail.com>
2020-11-12 11:00:39 +0300

    Contact field fix

--- application/views/contact_us/contact_edit_section1.php
+++ application/views/contact_us/contact_edit_section1.php
@@ -16,32 +16,24 @@
                                                   <div class="form-validation">
                                                      <form class="form-valide" action="<?php echo base_url('ContactContent/save_contact')?>" enctype="multipart/form-data" method="post" accept-charset="utf-8">
                                                         <input type="hidden" name="contact_id" value="<?= $edit_section1[0]['id']?>">
+                                                        <input type="hidden" class="form-control" id="mobile_number" name="mobile_number" value="<?php echo $edit_section1[0]['number']?>">
+                                                        <input type="hidden" class="form-control" id="email" name="email" value="<?php echo $edit_section1[0]['email']?>">
+
                                                         <div class="form-group row">
                                                            <label class="col-md-3 col-form-label" for="val-username">Name <span class="text-danger">*</span>
                                                            </label>
                                                            <div class="col-md-9">
-                                                              <input type="text" class="form-control" id="title" name="title" value="<?php echo $edit_section1[0]['title']?>">
+                                                              <input type="text" class="form-control" value="<?php echo $edit_section1[0]['title']?>" disabled>
+                                                              <input type="hidden" class="form-control" id="title" name="title" value="<?php echo $edit_section1[0]['title']?>">
                                                            </div> 
                                                         </div>
-                                                        <div class="form-group row">
-                                                           <label class="col-md-3 col-form-label" for="val-username">Mobile Number <span class="text-danger">*</span>
-                                                           </label>
-                                                           <div class="col-md-9">
-                                                              <input type="number" class="form-control" id="mobile_number" name="mobile_number" value="<?php echo $edit_section1[0]['number']?>">
-                                                           </div> 
-                                                        </div>
-                                                        <div class="form-group row">
-                                                           <label class="col-md-3 col-form-label" for="val-username">Email<span class="text-danger">*</span>
-                                                           </label>
-                                                           <div class="col-md-9">
-                                                              <input type="email" class="form-control" id="email" name="email" value="<?php echo $edit_section1[0]['email']?>">
-                                                           </div> 
-                                                        </div>
+                                                        
                                                         <div class="form-group row">
                                                            <label class="col-md-3 col-form-label" for="val-password">Contact Desciption <span class="text-danger">*</span>
                                                            </label>
                                                            <div class="col-md-9">
-                                                              <textarea class="form-control" name="description"><?php echo $edit_section1[0]['description']?></textarea>
+                                                              <textarea class="form-control" disabled><?php echo $edit_section1[0]['description']?></textarea>
+                                                              <input type="hidden" class="form-control" name="description" value="<?php echo $edit_section1[0]['description']?>">
                                                            </div>
                                                         </div>
                                                         <div class="form-group row">
