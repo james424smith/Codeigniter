0d87616f6bdcc71e050d6759890b5f6112a27bd1
ukey-ltd <68227576+ukey-ltd@users.noreply.github.com>
2020-11-17 18:00:53 +0100

    Update about_heelp.php

--- application/views/Front/about_heelp.php
+++ application/views/Front/about_heelp.php
@@ -22,15 +22,15 @@
 			<div class="row">
 				<div class="col-md-12 about_heelp_cont">
 					<p><b>Heelp</b> est une application qui a été conçue en collaboration avec des 
-						étudiants dans le but de répondre à trois principaux besoins de l’étudiant.</p>
+						étudiants dans le but de répondre à trois principaux besoins d'un’étudiant.</p>
 					<p><b>Heelp</b> permet aux étudiants de :</p>
 					<ul >
-						<li>1-S’entraider pour pouvoir faire leurs devoirs ou projets scolaires à distance
+						<li>S’entraider pour pouvoir faire leurs devoirs ou projets scolaires et universitaires à distance
 						</li>
-						<li>2-Développer leurs connaissances et compétences en échangeant avec d’autres étudiants </li>
-						<li>3-Gagner l’argent depuis leurs domiciles simplement en aidant d’autres étudiants </li>
+						<li>Développer leurs connaissances et compétences en échangeant avec d’autres étudiants </li>
+						<li>Gagner l’argent depuis leurs domiciles simplement en aidant d’autres étudiants </li>
 					</ul>
-					<p>Contacter l’équipe Heelp par mail</p>
+					<p></p>
 					<span>contact@heelp.fr</span>
 				</div>
 			</div>
